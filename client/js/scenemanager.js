dcaeace650df76258c402b9259e3bdcd14948837
Justin Brower <justin_brower@brown.edu>
2015-03-19 01:30:45 -0400

    added documentation / finished scene manager prototype

--- client/js/scenemanager.js
+++ client/js/scenemanager.js
@@ -4,7 +4,24 @@
   *		written in javascript. This allows for games to be made in a 
   *		compartmentalized way, without focusing on transitions or game flow.
   *
-  *     TODO: Add a better description.
+  *		Relies On:
+  *				- jQuery (1.6+)
+  *				- Luck
+  *
+  *
+  *		Instance Methods:
+  *			  # Presents a scene object with the given scene ID.
+  *			- (void) presentScene(SceneID)
+  *
+  *					
+  *			  # Preloads a scene. This is essentially moot until Web Workers
+  * 		  # are implemented for background loading.
+  *			- (void) loadScene(Scene scene)
+  *		
+  *		Constructors:
+  *
+  *			  # SceneManager(contentDivID, scenes)
+  *			  [see below for documentation]
   *
   */
 
@@ -22,7 +39,8 @@ $.fn.exists = function () {
 *
 *					If: 
 *						- a div with this id does not exist, one will be created
-*						- a div with this id DOES exist, its contents will be cleared and used for the scene manager.
+*						- a div with this id DOES exist, and the class is set to 
+							'__scenebox__', its contents will be cleared and reused by the scene manager.
 *						- a NON DIV element with this id exists, an alert() will be presented.
 *
 *		scenes:
@@ -36,11 +54,24 @@ function SceneManager(contentDivID, scenes) {
 
 		this.contentDivID = contentDiv;
 		this.ANIM_LENGTH = 5;
-		if (document.getElementById(this.contentDivID) == null) {
+
+		var existingDiv = document.getElementById(this.contentDivID);
+
+		if (existingDiv == null) {
 			//create a div
 			this.contentDiv = document.createElement("div");
 			this.contentDiv.className = "__scenebox__";
 			document.body.appendChild(contentDiv);
+		} else {
+			//is it a div
+			if (existingDiv.className == "__scenebox__" && existingDiv.className == "div") {
+				//we found love in a hopeless place
+				this.contentDiv = existingDiv;
+			} else {
+				alert("[scenemanager.js] Error: Couldn't load content div - tag must be a div and class must be __scenebox__");
+				//to avoid getting into sticky situations, refuse to load the scenes.
+				return;
+			}
 		}
 
 		this.scenes = scenes;
@@ -50,7 +81,7 @@ function SceneManager(contentDivID, scenes) {
 * Loads a scene into the scenemanager's content div. If there is a scene currently loaded,
 * the scene will be ejected using a left to right animation.
 */
-SceneManager.prototype.loadScene = function(sceneID) {
+SceneManager.prototype.presentScene = function(sceneID) {
 
 	var scene = this.scenes[sceneID];
 
@@ -91,14 +122,33 @@ SceneManager.prototype.loadScene = function(sceneID) {
 		});
 	}
 
-	jQuery(newScene).animate({{left: 0}}, {
+	jQuery(newScene).animate({left: 0}, {
 			queue: false,
 			duration: this.ANIM_LENGTH,
 			complete: function() {
 				if (scene.onSceneLoad != null) {
-					this.onSceneLoad();
+					this.onPresent();
 					this.activeScene = scene;
 				}
 			}
 	});
+};
+
+
+
+/**
+ * Preloads the resources associated with a given scene.
+ */
+SceneManager.prototype.loadScene = function(sceneID) {
+
+	var scene = this.scenes[sceneID];
+
+	if (scene.preload) {
+		//custom preloading is available for this scene
+		//TODO: use web workers to make this REAL fast. As of right now, without threads this is useless.
+		scene.preload();
+	}
 }
+
+
+
