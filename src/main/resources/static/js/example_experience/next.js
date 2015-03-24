ffbf2e603a5a2962e90feb50bec1934f702acdcb
Justin Brower <justin_brower@brown.edu>
2015-03-24 19:26:34 -0400

    Cleaning up sample scene

---
+++ src/main/resources/static/js/example_experience/next.js
@@ -0,0 +1,12 @@
+var exported_scene = {
+	onPresent : function(scene) { 
+		alert("Dynamically loading this scene!");
+		scene.searchElement("img").css("border 5px gray;");
+	},
+	onDestroy: function(scene) {
+		alert("Time for the next scene!");
+	},
+	getHTML : function() {
+		return null
+	}
+};
