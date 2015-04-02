bd28bf81df3a0f9b06c7bdd723c562ff7c8e8239
Justin Brower <justin_brower@brown.edu>
2015-04-02 01:31:08 -0400

    Slight changes to example experience.

--- examples/example_experience/mainScene.js
+++ examples/example_experience/mainScene.js
@@ -4,24 +4,11 @@ var exported_scene = {
 		alert("The scene happened!");
 	},
 	onDestroy: function() {
-		alert("Time for the next scene!");
 	},
 	getHTML : function() {
 		return "mainScene.html"
 	}
 };
-
-
-    var nextScene = function() {
-        console.log("KLJHSDGJKHSDKJGHSDKLG");
-        if (this.scene == null) {
-            console.error("Didn't get reference to scene.");
-        } else {
-            console.log(this.scene);
-        }
-
-        exported_scene.scene.manager.presentScene("next");
-    }
 
 
 
