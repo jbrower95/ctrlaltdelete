d7642508c4e3e59767c5e4d8ffb33a1d17df61ba
Justin Brower <justin_brower@brown.edu>
2015-04-16 18:00:35 -0400

    gabes room?

--- examples/ctrlaltdel/gabeshouse.scene/gabeshouse.js
+++ examples/ctrlaltdel/gabeshouse.scene/gabeshouse.js
@@ -6,7 +6,11 @@ var exported_scene = {
 
     onPresent : function() {
 
-        this.searchContent("#gabe").animate({left: "12%", bottom: "25%"});
+        this.searchContent("#gabe").animate({left: "12%", bottom: "25%"}, 300, function() {
+            setTimeout($.proxy(function() {
+                this.src = "images/gabe_back.png";
+            }, this), 300);
+        });
     },
 
     onDestroy : function() {
