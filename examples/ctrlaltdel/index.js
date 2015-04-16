38f7e343ce070a4ac5174ff4cd2b6a64e5e842de
Justin Brower <justin_brower@brown.edu>
2015-04-16 14:54:43 -0400

    moved shit around

--- examples/ctrlaltdel/index.js
+++ examples/ctrlaltdel/index.js
@@ -4,11 +4,12 @@ var scenes = {};
 
 
 SceneManager.initialize("content");
-Scene.load("copyright.scene/copyright.js", startGame);
+Scene.load("copyright.scene/copyright.js");
 Scene.load("credits.scene/credits.js");
-Scene.load("mainmenu.scene/mainMenu.js");
+Scene.load("mainmenu.scene/mainMenu.js", startGame);
 Scene.load("introcut.scene/introcut.js");
 
+
 function startGame() {
-    SceneManager.getSharedInstance().presentScene("copyright");
+    SceneManager.getSharedInstance().presentScene("mainMenu");
 }
