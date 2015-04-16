b828f1484d3fef72d5655fa601fd12f1e00c9e59
Justin Brower <justin_brower@brown.edu>
2015-04-16 13:58:28 -0400

    refactor refactor refactor refactor refactor refactor

--- examples/ctrlaltdel/index.js
+++ examples/ctrlaltdel/index.js
@@ -7,7 +7,7 @@ SceneManager.initialize("content");
 Scene.load("copyright.scene/copyright.js", startGame);
 Scene.load("credits.scene/credits.js");
 Scene.load("mainMenu.scene/mainMenu.js");
-Scene.load("main.scene/main.js");
+Scene.load("introcut.scene/introcut.js");
 
 function startGame() {
     SceneManager.getSharedInstance().presentScene("copyright");
