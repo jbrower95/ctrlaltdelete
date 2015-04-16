09352691c82e8d3cadf403d9ae63f3f8b9dd0a76
Justin Brower <justin_brower@brown.edu>
2015-04-16 17:17:03 -0400

    anus

--- examples/ctrlaltdel/introcut.scene/introcut.js
+++ examples/ctrlaltdel/introcut.scene/introcut.js
@@ -1,11 +1,38 @@
 var exported_scene = {
     id : "introcut",
-    onPresent : function() {
-    	SceneManager.getSharedInstance().presentScene("windows95");
-    },
 	getHTML : function() {
 		return "introcut.scene/introcut.html"
-	}
+	},
+    onPresent : function() {
+
+        this.searchContent("caption").hide();
+
+        setTimeout(this.searchContent("caption").show, 2000);
+
+        this.searchContent("#gabe").css("left", "35%");
+        this.searchContent("#gabe").css("bottom", "0");
+        var duration = 4000;
+
+        function walkAway() {
+            this.searchContent("#gabe").animate({left: "70%", bottom: "40%"}, duration, $.proxy(function() {
+                this.searchContent("#gabe").hide(300, $.proxy(function() {
+                    setTimeout($.proxy(transition,this), 200);
+                }, this));
+            }, this));
+        }
+
+        function transition(){
+            $(this.searchContent("#introCut")).animate({"left" : "-100%"}, 200, $.proxy(function() {
+                SceneManager.getSharedInstance().presentScene("windows95");
+            }, this));
+        }
+
+        this.searchContent("#gabe").animate({left: "48%"}, 200, $.proxy(walkAway, this));
+    },
+
+    onDestroy : function() {
+
+    }
 };
 
 
