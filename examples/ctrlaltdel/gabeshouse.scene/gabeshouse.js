211da654efd42270d071be738e910b8957c45b92
Justin Brower <justin_brower@brown.edu>
2015-04-16 18:27:15 -0400

    gabesroom is done

--- examples/ctrlaltdel/gabeshouse.scene/gabeshouse.js
+++ examples/ctrlaltdel/gabeshouse.scene/gabeshouse.js
@@ -6,10 +6,26 @@ var exported_scene = {
 
     onPresent : function() {
 
-        this.searchContent("#gabe").animate({left: "12%", bottom: "25%"}, 300, function() {
-            setTimeout($.proxy(function() {
-                this.src = "images/gabe_back.png";
-            }, this), 300);
+        this.searchContent(".caption").hide();
+
+        this.searchContent(".caption").eq(0).show();
+
+        var scene = this;
+        var gabe = this.searchContent("#gabe");
+
+        gabe.animate({left: "12%", bottom: "25%"}, 300, function() {
+            setTimeout(function() {
+                console.log(gabe);
+                gabe.attr("src", "images/gabe_back.png");
+                setTimeout(function() {
+                    scene.searchContent(".caption").eq(0).hide();
+                    scene.searchContent(".caption").eq(1).show();
+                    gabe.animate({left: "5%", bottom: "58%"}, 800);
+                    gabe.hide(2000, function() {
+                        SceneManager.getSharedInstance().presentScene("windows95");
+                    });
+                }, 5000);
+            }, 600);
         });
     },
 
