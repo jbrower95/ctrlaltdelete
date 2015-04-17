1723f61de57abdc177e6e7bd431590deeb020ddf
jbrower95 <jbrower95@gmail.com>
2015-04-17 13:57:41 -0400

    CLIPPY IS ALIVE

--- examples/ctrlaltdel/windows95.scene/windows95.js
+++ examples/ctrlaltdel/windows95.scene/windows95.js
@@ -4,6 +4,11 @@ var exported_scene = {
 		var manager = new WindowManager("desktop");
 		var win = manager.inflate("taskManager");
 		manager.addWindow(win);
+		 clippy.load('Clippy', function(agent) {
+		        agent.show();
+		        agent.moveTo(200,200);
+		        agent.speak('Test');
+		    });
 	},
 	onDestroy: function() {
 
