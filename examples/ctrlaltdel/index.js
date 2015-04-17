596f1398e4ffa63a94e0cde9d30809ffb6b54df4
Arielle Chapin <arielle_chapin@brown.edu>
2015-04-17 14:34:56 -0400

    so draggable

--- examples/ctrlaltdel/index.js
+++ examples/ctrlaltdel/index.js
@@ -7,7 +7,7 @@ SceneManager.initialize("content");
 Scene.load("copyright.scene/copyright.js", startGame);
 Scene.load("scores.scene/scores.js");
 Scene.load("credits.scene/credits.js");
-Scene.load("mainMenu.scene/mainMenu.js");
+Scene.load("mainmenu.scene/mainMenu.js");
 Scene.load("introcut.scene/introcut.js");
 Scene.load("windows95.scene/windows95.js");
 Scene.load("gabeshouse.scene/gabeshouse.js");
