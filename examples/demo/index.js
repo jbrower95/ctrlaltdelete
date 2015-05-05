033a7931acfd394cd04a85f343bec9921e8bb1f7
Arielle <arielle.bryn@gmail.com>
2015-05-05 02:25:15 -0400

    Separate css files

--- examples/demo/index.js
+++ examples/demo/index.js
@@ -9,4 +9,5 @@ SceneManager.initialize("content");
 
 Scene.load('slide1.scene/slide1.js').then(function() {SceneManager.getSharedInstance().presentScene('slide1')});
 Scene.load('slide2.scene/slide2.js');
+Scene.load('slide3.scene/slide3.js');
 
